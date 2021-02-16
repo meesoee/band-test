9a17845862b0a496ed15d9ce20fd3e7809ad0e43
meesoee <55368566+meesoee@users.noreply.github.com>
2021-02-17 01:55:26 +0900

    URL
    change

--- docs/js/share.js
+++ docs/js/share.js
@@ -21,14 +21,14 @@ function shareKakao() {
       title: '밴드 적성 검사 - 나의 결과는?',
       description: '나와 잘 맞는 악기가 무엇인지 테스트해보세요!',
       link: {
-        webUrl: 'https://meesoee.github.com/band-test'
+        webUrl: 'https://meesoee.github.io/band-test/'
       }
     },
     buttons: [
       {
         title: '나도 테스트하기',
         link: {
-          webUrl: 'https://meesoee.github.com/band-test'
+          webUrl: 'https://meesoee.github.io/band-test/'
         }
       }
     ]
