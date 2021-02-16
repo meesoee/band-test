d8b81c4e27f0dd5637dfe05e74b5e254c0a47c65
meesoee <55368566+meesoee@users.noreply.github.com>
2021-02-17 02:12:20 +0900

    change the domain
    work plz

--- docs/js/share.js
+++ docs/js/share.js
@@ -21,16 +21,16 @@ function shareKakao() {
       title: '밴드 적성 검사 - 나의 결과는?',
       description: '나와 잘 맞는 악기가 무엇인지 테스트해보세요!',
       link: {
-        mobileWebUrl: 'https://meesoee.github.io/band-test/',
-        webUrl: 'https://meesoee.github.io/band-test/'
+        mobileWebUrl: 'https://meesoee.band-test',
+        webUrl: 'https://meesoee.band-test'
       }
     },
     buttons: [
       {
         title: '나도 테스트하기',
         link: {
-          mobileWebUrl: 'https://meesoee.github.io/band-test/',
-          webUrl: 'https://meesoee.github.io/band-test/'
+          mobileWebUrl: 'https://meesoee.band-test',
+          webUrl: 'https://meesoee.band-test'
         }
       }
     ]
